1e50e1210fbaa3ffe6b7f136cba0f929c768ee71
Xaofly Sho <shaoxiaof@hotmail.com>
2022-01-11 13:51:17 +0800

    Demo

--- XSConfettiDemo/ViewController.swift
+++ XSConfettiDemo/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonClick(_ sender: UIButton) {
+        let confettiView = XSConfettiView(frame: self.view.bounds)
+        self.view.addSubview(confettiView)
+    }
 }
 
