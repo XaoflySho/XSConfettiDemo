e8127a1d4b849cb1ed5d31b38ba4094da58c6366
Xaofly Sho <shaoxiaof@hotmail.com>
2022-01-11 10:55:48 +0800

    Initial Commit

---
+++ XSConfettiDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XSConfettiDemo
+//
+//  Created by 邵晓飞 on 2022/1/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
